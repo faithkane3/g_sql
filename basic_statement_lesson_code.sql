37cbad1c36bb47b6a5a4f06bee8543b09cdfa344
Faith Kane <faith.x.kane@gmail.com>
2021-06-22 16:37:10 -0500

    Commit lesson shell and code updated wtih numbers

--- basic_statement_lesson_code.sql
+++ basic_statement_lesson_code.sql
@@ -1,47 +1,59 @@
--- Select the database.
+-- A. Basic SELECT Statement
+
+-- 1. Select the database.
 USE fruits_db;
 
--- Inspect the columns and data types from a table.
+-- 2. Inspect the columns and data types from a table.
 DESCRIBE fruits;
 
--- Another way to Inspect the columns and data types from a table.
+-- 3. Another way to Inspect the columns and data types from a table.
 SHOW COLUMNS
 FROM fruits;
 
--- Return all of the rows and columns from a table.
+-- 4. Return all of the rows and columns from a table.
 SELECT *
 FROM fruits;
 
--- Select specific column(s) and all of the rows from those column(s).
+-- 5. Select specific column(s) and all of the rows from those column(s).
 SELECT name
 FROM fruits;
 
 SELECT name, quantity
 FROM fruits;
 
--- Use chiplotle database to demo a db with duplicates.
+-- B. SELECT DISTINCT Statement
+
+-- 1. Use chiplotle database to demo a db with duplicates.
 USE chipotle;
 
--- Inspect the columns and data types from a table.
+-- 2. Inspect the columns and data types from a table.
 DESCRIBE orders;
 
--- Return all of the rows and columns from a table. (4622 records)
+-- 3. Return all of the rows and columns from a table. (4622 records)
 SELECT *
 FROM orders;
 
--- Select specific column(s) and all of the rows from those column(s). (4622 records)
+/* 
+4. Select specific column(s) and all of the rows from those column(s). 
+(4622 records)
+*/
 SELECT item_name
 FROM orders;
 
 SELECT item_name, item_price
 FROM orders;
 
--- Return only the unique values from a column using the DISTINCT keyword (50 records)
+/*
+5. Return only the unique values from a column using the DISTINCT keyword 
+(50 records returned)
+*/
 SELECT DISTINCT item_name
 FROM orders;
 
+-- C. Filter Return Sets Using WHERE
+
 /*
-Filter so that only records with the value 'Chicken Bowl' in item_name are returned.
+1. Filter so that only records with the value 'Chicken Bowl' in item_name are returned.
 (726 records returned)
 */
 
@@ -49,23 +61,25 @@ SELECT *
 FROM orders
 WHERE item_name = 'Chicken Bowl';
 
--- Why doesn't the query below run? Never forget this lesson!
+-- 2. Filter by item_price. Why doesn't the QUERY below run? 
 
 SELECT *
 FROM orders
 WHERE item_price = $4.45;
 
--- Filter using the primary key column; only one record will be returned because the value must be unique.
+/*
+3. Filter using the primary key column; only one record will be returned because the value must be unique.
+*/
 SELECT *
 FROM orders
 WHERE id = 15;
 
--- Filter using a WHERE clause with the BETWEEN & AND operators. (Returns 39 records)
+-- 4. Filter using a WHERE clause with the BETWEEN & AND operators. (Returns 39 records)
 SELECT *
 FROM orders 
 WHERE quantity BETWEEN 3 AND 5;
 
--- Filter using a WHERE statement >, <, <> operators.
+-- 5. Filter using a WHERE statement >, <, <> operators.
 SELECT *
 FROM orders 
 WHERE order_id > 1500;
@@ -74,11 +88,17 @@ SELECT *
 FROM orders
 WHERE quantity <> 1;
 
--- Create an alias for a column using the AS keyword. (Return 267 records)
+-- Create Alias Using AS
+
+-- 1. Create an alias for a column using the AS keyword. (Returns 267 records)
 
 SELECT 
 	item_name AS 'Multiple Item Order',
 	quantity AS Number
 FROM orders
 WHERE quantity >= 2;
-	
+
+/*
+Notice that if I have spaces in my column alias, I have to put it in single quotes.
+If I do not have a space in my colum alias, I do not have to put it in quotes.
+*/
--- select_lesson_shell.sql
+++ select_lesson_shell.sql
@@ -1,52 +1,49 @@
--- Basic SELECT Statement
+-- A. Basic SELECT Statement
 
--- Use the database fruits_db.
-
-
-
-
--- Inspect the columns and data types from a table.
+-- 1. Use the database fruits_db.
 
 
 
 
--- Another way to Inspect the columns and data types from a table.
+-- 2. Inspect the columns and data types from a table.
 
 
 
 
--- Return all of the rows and columns from a table.
+-- 3. Another way to Inspect the columns and data types from a table.
 
 
 
 
--- Select specific column(s) and all of the rows from those column(s).
+-- 4. Return all of the rows and columns from a table.
 
 
 
 
--- SELECT DISTINCT Statement
+-- 5. Select specific column(s) and all of the rows from those column(s).
 
 
 
 
--- Use chiplotle database to demo a db with duplicates.
+-- B. SELECT DISTINCT Statement
+
+-- 1. Use chiplotle database to demo a db with duplicates.
 
 
 
 
--- Inspect the columns and data types from the orders table.
+-- 2. Inspect the columns and data types from the orders table.
 
 
 
 
--- Return all of the rows and columns from a table. (4622 records returned)
+-- 3. Return all of the rows and columns from a table. (4622 records returned)
 
 
 
 
 /*
-Select specific column(s) and all of the rows from those column(s). 
+4. Select specific column(s) and all of the rows from those column(s). 
 (4622 records returned)
 */
 
@@ -54,46 +51,48 @@ Select specific column(s) and all of the rows from those column(s).
 
 
 /*
-Return only the unique values from a column using the DISTINCT keyword 
+5. Return only the unique values from a column using the DISTINCT keyword 
 (50 records returned)
 */
 
 
 
+-- C. Filter Return Sets Using WHERE
 
 /*
-Filter so that only records with the value 'Chicken Bowl' in item_name are returned.
+1. Filter so that only records with the value 'Chicken Bowl' in item_name are returned.
 (726 records returned)
 */
 
 
 
--- Filter by item_price. Why doesn't the QUERY below run? 
+-- 2. Filter by item_price. Why doesn't the QUERY below run? 
 
 
 
 
 /*
-Filter using the primary key column; only one record will be returned because the value must be unique.
+3. Filter using the primary key column; only one record will be returned because the value must be unique.
 */
 
 
 
--- Filter using a WHERE clause with the BETWEEN & AND operators. (Returns 39 records)
+-- 4. Filter using a WHERE clause with the BETWEEN & AND operators. (Returns 39 records)
 
 
 
 
--- Filter using a WHERE statement >, <, <> operators.
+-- 5. Filter using a WHERE statement >, <, <> operators.
 
 
 
 -- Create Alias Using AS
 
--- Create an alias for a column using the AS keyword. (Returns 267 records)
+-- 1. Create an alias for a column using the AS keyword. (Returns 267 records)
 
 
 
 /*
 Notice that if I have spaces in my column alias, I have to put it in single quotes.
 If I do not have a space in my colum alias, I do not have to put it in quotes.
+*/
