4c71c26eee206fe9f7dfd4eed7bea4dd64b3accf
Faith Kane <faith.x.kane@gmail.com>
2021-06-23 11:36:32 -0500

    Commit edits to where lesson.

---
+++ order_by_lesson_shell.sql
@@ -0,0 +1,43 @@
+##################### ORDER BY Clause #############################
+
+/*
+SELECT column_name
+FROM table
+ORDER BY column_name [ASC|DESC];
+*/
+-- Use chipotle database;
+USE chipotle;
+
+
+-- Return all of the items in the orders table and return the records in alphabetical order.
+
+
+
+
+-- Return all of the items in the orders table and return the records in reverse alphabetical order.
+
+
+
+
+-- Alias the item_name column and return in reverse alphabetical order.
+
+
+
+
+########################### Chaining ORDER BY Clauses #####################
+
+-- Alias the items and quantity columns and return in ascending order by the number and then the item.
+
+
+
+
+
+/*
+Alias the items and quantity columns and return in descending order by the number 
+and then the item in alphabetical order.
+*/
+
+
+
+
+
